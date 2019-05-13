7e6bb63f1e3b44e60c48e5d4aadb852dc6ef7a17
Kevin Smith <kevin@muny.us>
2019-05-13 10:56:03 -0400

    added livingroom

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { App } from './app'
 import * as fs from 'fs'
+import { TuyaConfigImporter } from './DeviceImporters/TuyaConfigImporter';
+import { TuyaProxyImporter } from './DeviceImporters/TuyaProxyImporter';
 
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
